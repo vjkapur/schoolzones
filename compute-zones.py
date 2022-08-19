c4fdf119166fd7755ce8f6ef5031200b2d609bc4
VJ Kapur <vj@intrical.us>
2022-08-19 10:01:49 -0400

    added output placeholder

--- compute-zones.py
+++ compute-zones.py
@@ -6,3 +6,5 @@ import geopandas as gpd
 school_grounds = gpd.read_file("data/Public_School_Grounds.geojson")
 streets = gpd.read_file("data/Street_Right_of_Way_Polygons.geojson")
 
+# schoolzones = gpd.GeoDataFrame()
+# schoolzones.to_file("schoolzones.geojson", driver='GeoJSON')
